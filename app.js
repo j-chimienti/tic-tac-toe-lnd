19941fdda1b50ab11ad01e50e0e408faf5dbf909
joe <jchimien@gmail.com>
2018-10-28 13:47:46 -0400

    c

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 
 });
 
-app.use('/order', express.static(path.join(__dirname, 'build')));
+app.use('/order', express.static(path.join(__dirname, 'build', 'static')));
 
 app.get('/order/:id', async (req, res, next) => {
 
