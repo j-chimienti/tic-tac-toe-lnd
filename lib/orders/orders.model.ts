9d1475a04c6231352ad8a3e5eba6f73b7e7b0a85
joe <jchimien@gmail.com>
2018-11-10 21:59:59 -0500

    t

--- lib/orders/orders.model.ts
+++ lib/orders/orders.model.ts
@@ -1,36 +1,120 @@
-interface BaseOrder {
-    id: string;
-    userId: string;
-}
-
-
-interface IOrder extends BaseOrder {
-
-    expirationTime: string | number;
-    invoiceTime: string | number;
-    currentTime: string | number;
-}
-
-interface IOrderDb extends BaseOrder {
-    expirationTime: Date;
-    invoiceTime: Date;
-    currentTime: Date;
-}
-
-export class Order implements IOrderDb {
+export class Order implements IOrder {
 
     public id;
     public userId;
     public expirationTime;
     public invoiceTime;
     public currentTime;
+    public url;
+    public posData;
+    public status;
+    public btcPrice;
+    public price;
+    public currency;
+    public btcPaid;
+    public btcDue;
+    public rate;
+    public exceptionStatus;
+    public buyerFields;
+    public transactionCurrency;
+    public paymentSubtotals;
+    public paymentTotals;
+    public amountPaid;
+    public exchangeRates;
 
-    constructor({id, userId, expirationTime, invoiceTime, currentTime}: IOrder) {
+    constructor({
+                    id,
+                    userId,
+                    expirationTime,
+                    invoiceTime,
+                    currentTime,
+                    url,
+                    posData,
+                    status,
+                    btcPrice,
+                    price,
+                    currency,
+                    btcPaid,
+                    btcDue,
+                    rate,
+                    exceptionStatus,
+                    buyerFields,
+                    transactionCurrency,
+                    paymentSubtotals,
+                    paymentTotals,
+                    amountPaid,
+                    exchangeRates,
+                }: IOrder) {
         this.id = id;
         this.userId = userId;
         this.expirationTime = new Date(expirationTime);
         this.invoiceTime = new Date(invoiceTime);
         this.currentTime = new Date(currentTime);
+        this.url = url;
+        this.posData = posData;
+        this.status = status;
+        this.btcPrice = btcPrice;
+        this.price = price;
+        this.currency = currency;
+        this.invoiceTime = invoiceTime;
+        this.expirationTime = expirationTime;
+        this.currentTime = currentTime;
+        this.btcPaid = btcPaid;
+        this.btcDue = btcDue;
+        this.rate = rate;
+        this.exceptionStatus = exceptionStatus;
+        this.buyerFields = buyerFields;
+        this.transactionCurrency = transactionCurrency;
+        this.paymentSubtotals = paymentSubtotals;
+        this.paymentTotals = paymentTotals;
+        this.amountPaid = amountPaid;
+        this.exchangeRates = exchangeRates;
+        this.userId = userId;
 
     }
 }
+
+
+interface PaymentSubtotals {
+    BTC_LightningLike: number;
+}
+
+interface PaymentTotals {
+    BTC_LightningLike: number;
+}
+
+interface BTC {
+    BTC: number;
+}
+
+interface ExchangeRates {
+    BTC: BTC;
+}
+
+interface IOrder {
+    // _id: string;
+    id: string;
+    url?: any;
+    posData?: any;
+    status: string;
+    btcPrice?: any;
+    price: number;
+    currency: string;
+    invoiceTime: number;
+    expirationTime: number;
+    currentTime: number;
+    btcPaid?: any;
+    btcDue?: any;
+    rate: number;
+    exceptionStatus: boolean;
+    buyerFields?: any;
+    transactionCurrency?: any;
+    paymentSubtotals: PaymentSubtotals;
+    paymentTotals: PaymentTotals;
+    amountPaid: string;
+    exchangeRates: ExchangeRates;
+    userId: string;
+}
+
+
+
